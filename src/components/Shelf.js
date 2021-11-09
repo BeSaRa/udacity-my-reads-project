3dc6598b37f504cc0d452762b215265716f03a43
BeSaRa <besara@windowslive.com>
2021-11-10 00:53:22 +0300

    feat: Split main page to Shelves and subcomponents
    1- split main page to Shelves component
    2- add loader image to display it till getting the date from backend for the first time
    3- create Shelf and Book component (still need some work)

---
+++ src/components/Shelf.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+import Book from "./Book";
+
+class Shelf extends Component {
+    render() {
+        const {books, title} = this.props.shelf;
+        return <div className="bookshelf">
+            <h2 className="bookshelf-title">{title}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {books.map(book => <Book book={book} key={book.title}/>)}
+                </ol>
+            </div>
+        </div>;
+    }
+}
+
+export default Shelf;
---
+++ src/components/Shelves.js
@@ -0,0 +1,80 @@
+import React, {Component} from "react";
+import Shelf from "./Shelf";
+import * as BookAPI from './../BooksAPI';
+import loaderImage from '../images/Loader.gif';
+
+class Shelves extends Component {
+    state = {
+        loaded: false,
+        shelvesByOrder: ['currentlyReading', 'wantToRead', 'read'],
+        shelves: {
+            wantToRead: {
+                title: 'Want to read',
+                books: []
+            },
+            currentlyReading: {
+                title: 'Currently Reading',
+                books: []
+            },
+            read: {
+                title: 'Read',
+                books: []
+            }
+        }
+    };
+
+    componentDidMount() {
+        BookAPI.getAll()
+            .then((books) => {
+                this.distributeBooksOnShelves(books);
+                this.setState((prevState) => ({...prevState, loaded: true}))
+            })
+    }
+
+    /**
+     * @description add book to shelf
+     * @param {string} shelf
+     * @param book - book model
+     */
+    addBookToShelf(shelf, book) {
+        this.setState((prevState) => {
+            return {
+                shelves: {
+                    ...prevState.shelves,
+                    [shelf]: {
+                        ...prevState.shelves[shelf],
+                        books: prevState.shelves[shelf].books.concat(book)
+                    }
+                }
+            }
+        })
+    }
+
+    /**
+     * @description distribute books for each shelf
+     * @param {Array} books
+     */
+    distributeBooksOnShelves(books) {
+        books.forEach(book => this.addBookToShelf(book.shelf, book));
+    }
+
+    render() {
+        const shelves = <div>{this.state.shelvesByOrder.map(shelf => (
+            <Shelf key={shelf} shelf={this.state.shelves[shelf]}/>))}</div>
+        const loading = <div className="loader-wrapper">
+            <div style={{
+                background: `url(${loaderImage}) center center no-repeat`,
+                width: 250,
+                height: 150
+            }}/>
+            <span className="loading-word">Loading...</span>
+        </div>
+        return (
+            <div className="list-books-content">
+                {((this.state.loaded && (shelves)) || loading)}
+            </div>
+        )
+    }
+}
+
+export default Shelves;
