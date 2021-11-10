1271e7273267a08c4c031f23632608124722fe57
BeSaRa <besara@windowslive.com>
2021-11-10 11:25:49 +0300

    refactor: Change Shelf to be functional component

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,18 +1,15 @@
-import React, {Component} from "react";
+import React from "react";
 import Book from "./Book";
 
-class Shelf extends Component {
-    render() {
-        const {shelf: {books, title}, moveBookCallback} = this.props;
-        return <div className="bookshelf">
-            <h2 className="bookshelf-title">{title} - ({books.length})</h2>
-            <div className="bookshelf-books">
-                <ol className="books-grid">
-                    {books.map(book => <Book moveBookCallback={moveBookCallback} book={book} key={book.id}/>)}
-                </ol>
-            </div>
-        </div>;
-    }
+function Shelf({shelf: {books, title}, moveBookCallback}) {
+    return <div className="bookshelf">
+        <h2 className="bookshelf-title">{title} - ({books.length})</h2>
+        <div className="bookshelf-books">
+            <ol className="books-grid">
+                {books.map(book => <Book moveBookCallback={moveBookCallback} book={book} key={book.id}/>)}
+            </ol>
+        </div>
+    </div>;
 }
 
 export default Shelf;
